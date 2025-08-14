5d0d2cfbdf6ca9af54c81456f153b355470803ef
Boomexe <aptenathan@gmail.com>
2025-08-13 19:03:38 -0700

    animated haptic tab

--- client/components/HapticTab.tsx
+++ client/components/HapticTab.tsx
@@ -1,18 +1,65 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
-import * as Haptics from 'expo-haptics';
+import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
+import { PlatformPressable } from "@react-navigation/elements";
+import * as Haptics from "expo-haptics";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  runOnJS,
+} from "react-native-reanimated";
 
 export function HapticTab(props: BottomTabBarButtonProps) {
+  const opacity = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
+  const handlePressIn = (ev: any) => {
+    "worklet";
+    opacity.value = withTiming(0.5, { duration: 100 });
+
+    // Use runOnJS to call haptics from worklet
+    runOnJS(() => {
+      if (process.env.EXPO_OS === "ios") {
+        console.log("Tab pressed");
+      }
+    })();
+
+    // Call original onPressIn
+    if (props.onPressIn) {
+      runOnJS(props.onPressIn)(ev);
+    }
+  };
+
+  const handlePressOut = (ev: any) => {
+    "worklet";
+    opacity.value = withTiming(1, { duration: 100 });
+
+    if (props.onPressOut) {
+      runOnJS(props.onPressOut)(ev);
+    }
+  };
+
+  const handlePress = (ev: any) => {
+    if (process.env.EXPO_OS === "ios") {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    }
+
+    props.onPress?.(ev);
+  };
+
   return (
-    <PlatformPressable
-      {...props}
-      onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        }
-        props.onPressIn?.(ev);
-      }}
-    />
+    <Animated.View style={animatedStyle}>
+      {/* NOTE: can replace this with normal pressable */}
+      <PlatformPressable
+        {...props}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+        onPress={handlePress}
+      />
+    </Animated.View>
   );
 }
