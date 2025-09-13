a5e3c722257d14bd16a1cb5ae8b5c7287b227adc
Boomexe <aptenathan@gmail.com>
2025-09-13 15:21:38 -0700

    added some nice animations to auth screens

--- client/app/(auth)/sign-in-or-up.tsx
+++ client/app/(auth)/sign-in-or-up.tsx
@@ -8,6 +8,11 @@ import { useRouter } from "expo-router";
 import { theme } from "@/styles/theme";
 import ThemedText from "@/components/ThemedText";
 import BouncingCircles from "@/components/BouncingCircles";
+import Animated, {
+  FadeIn,
+  FadeInDown,
+  FadeInUp,
+} from "react-native-reanimated";
 
 const SignInOrUpScreen = () => {
   const router = useRouter();
@@ -15,29 +20,37 @@ const SignInOrUpScreen = () => {
     <BackgroundView withSafeArea withScreenPadding>
       <BouncingCircles />
       <View style={{ flex: 1, alignItems: "center" }}>
-        <ThemedText style={styles.header}>Welcome to Ambien 👋</ThemedText>
-        <ThemedText style={globalStyles.mutedText}>
-          Sign in or create an account to continue
-        </ThemedText>
+        <Animated.View entering={FadeInUp.duration(2000).delay(200)}>
+          <ThemedText style={styles.header}>Welcome to Ambien 👋</ThemedText>
+        </Animated.View>
+        <Animated.View entering={FadeInUp.duration(1700).delay(500)}>
+          <ThemedText style={globalStyles.mutedText}>
+            Sign in or create an account to continue
+          </ThemedText>
+        </Animated.View>
       </View>
-      <TextButton
-        title="Create an Account"
-        onPress={() => router.push("/sign-up")}
-        style={{ marginBottom: 12 }}
-        textStyle={{
-          fontWeight: theme.fontWeight.bold,
-          fontSize: theme.fontSize.lg,
-        }}
-      />
-      <TextButton
-        title="Sign in"
-        onPress={() => router.push("/sign-in")}
-        variant="secondary"
-        textStyle={{
-          fontWeight: theme.fontWeight.bold,
-          fontSize: theme.fontSize.lg,
-        }}
-      />
+      <Animated.View entering={FadeInDown.duration(2000).delay(800)}>
+        <TextButton
+          title="Create an Account"
+          onPress={() => router.push("/sign-up")}
+          style={{ marginBottom: 12 }}
+          textStyle={{
+            fontWeight: theme.fontWeight.bold,
+            fontSize: theme.fontSize.lg,
+          }}
+        />
+      </Animated.View>
+      <Animated.View entering={FadeInDown.duration(2000).delay(1100)}>
+        <TextButton
+          title="Sign in"
+          onPress={() => router.push("/sign-in")}
+          variant="secondary"
+          textStyle={{
+            fontWeight: theme.fontWeight.bold,
+            fontSize: theme.fontSize.lg,
+          }}
+        />
+      </Animated.View>
     </BackgroundView>
   );
 };
