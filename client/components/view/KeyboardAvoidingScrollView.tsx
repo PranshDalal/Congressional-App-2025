e3819cb4fafce76ec37341dfc00f726056edad86
Boomexe <aptenathan@gmail.com>
2025-09-13 11:20:13 -0700

    removed keyboard avoiding scroll view

--- client/components/view/KeyboardAvoidingScrollView.tsx
+++ client/components/view/KeyboardAvoidingScrollView.tsx
@@ -1,30 +1,30 @@
-import {
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  Keyboard,
-  ViewProps,
-} from "react-native";
-import React from "react";
+// import {
+//   KeyboardAvoidingView,
+//   Platform,
+//   ScrollView,
+//   Keyboard,
+//   ViewProps,
+// } from "react-native";
+// import React from "react";
 
-const KeyboardAvoidingScrollView = ({ children }: ViewProps) => {
-  return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 20 : 0}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-    >
-      <ScrollView
-        style={{ flex: 1 }}
-        contentContainerStyle={{ flexGrow: 1 }}
-        keyboardShouldPersistTaps="handled"
-        showsVerticalScrollIndicator={true}
-        onScrollBeginDrag={() => Keyboard.dismiss()}
-      >
-        {children}
-      </ScrollView>
-    </KeyboardAvoidingView>
-  );
-};
+// const KeyboardAvoidingScrollView = ({ children }: ViewProps) => {
+//   return (
+//     <KeyboardAvoidingView
+//       style={{ flex: 1 }}
+//       keyboardVerticalOffset={Platform.OS === "ios" ? 20 : 0}
+//       behavior={Platform.OS === "ios" ? "padding" : "height"}
+//     >
+//       <ScrollView
+//         style={{ flex: 1 }}
+//         contentContainerStyle={{ flexGrow: 1 }}
+//         keyboardShouldPersistTaps="handled"
+//         showsVerticalScrollIndicator={true}
+//         onScrollBeginDrag={() => Keyboard.dismiss()}
+//       >
+//         {children}
+//       </ScrollView>
+//     </KeyboardAvoidingView>
+//   );
+// };
 
-export default KeyboardAvoidingScrollView;
+// export default KeyboardAvoidingScrollView;
